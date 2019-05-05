e707a239e11dc07cd215fc5934f5d4f89c347057
Alexandre Faria <alexandreffaria@me.com>
2019-05-05 16:17:31 -0300

    form de contato agora ta funcionando, não sei o que estava quebrado

--- php/mail.php
+++ php/mail.php
@@ -1,20 +1,28 @@
 <?php
-$subject    = 'E-mail do site Dia do Arauto'; // Subject of your email
-$to         = 'contato@diadoarauto.com.br'; //Your e-mail address
-$headers    = 'MIME-Version: 1.0' . "\r\n" .
-              'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$message    = 'Nome: ' . $_POST['name'] . ' <br/>' .
-              'E-mail: ' . $_POST['email'] . ' <br/>' .
-              'Telefone: ' . $_POST['phone'] . ' <br/>' .
-              'Site: ' . $_POST['website'] . ' <br/>' .
-              'Mensagem: ' . $_POST['message'];
 
-if (@mail($to, $subject, $message, $headers))
-{
-  echo 'sent';
-}
-else
-{
-  echo 'failed';
-}
+
+
+  $subject    = 'Formulário do site Dia do Arauto'; // Subject of your email
+
+  $to         = 'contato@diadoarauto.com.br'; //Your e-mail address
+
+  $headers    = 'MIME-Version: 1.0' . "\r\n" .
+                'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+  $message    = 'Nome: ' . $_POST['name'] . ' <br/>' .
+                'E-mail: ' . $_POST['email'] . ' <br/>' .
+                'Telefone: ' . $_POST['phone'] . ' <br/>' .
+                'Site: ' . $_POST['website'] . ' <br/>' .
+                'Mensagem: ' . $_POST['message'];
+
+
+  if(mail($to, $subject, $message, $headers)){
+      header("Location: ../sucesso.html");
+  }
+  else{
+    header("Location: ../falha.html");
+  }
+
+  //echo "optimize";
+
 ?>
