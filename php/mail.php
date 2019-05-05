8adc33e8d1837e434a8680c3e8126d7489695fdd
Alexandre Faria <alexandreffaria@me.com>
2019-05-05 14:39:16 -0300

    Correção no form de contato que não estava funcionando :x

--- php/mail.php
+++ php/mail.php
@@ -1,13 +1,13 @@
 <?php
-$subject    = 'E-mail from example'; // Subject of your email
-$to         = 'email@example.com'; //Your e-mail address
+$subject    = 'E-mail do site Dia do Arauto'; // Subject of your email
+$to         = 'contato@diadoarauto.com.br'; //Your e-mail address
 $headers    = 'MIME-Version: 1.0' . "\r\n" .
               'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$message    = 'Name: ' . $_POST['name'] . ' <br/>' .
+$message    = 'Nome: ' . $_POST['name'] . ' <br/>' .
               'E-mail: ' . $_POST['email'] . ' <br/>' .
-              'Phone: ' . $_POST['phone'] . ' <br/>' .
-              'Website: ' . $_POST['website'] . ' <br/>' .
-              'Message: ' . $_POST['message'];
+              'Telefone: ' . $_POST['phone'] . ' <br/>' .
+              'Site: ' . $_POST['website'] . ' <br/>' .
+              'Mensagem: ' . $_POST['message'];
 
 if (@mail($to, $subject, $message, $headers))
 {
